f04857407263578ea3af03867061d19deb09ea2f
Kelkar <AKELKAR1@jaguarlandrover.com>
2024-02-08 07:16:00 +0000

    citi bank code changes

--- exercises/citi_bank.py
+++ exercises/citi_bank.py
@@ -11,18 +11,18 @@ browser.get("https://www.online.citibank.co.in/")
 browser.maximize_window()
 browser.implicitly_wait(20)
 
-browser.find_element(By.XPATH,"//a[@class='newclose']").click()
-browser.find_element(By.XPATH,"//a[@class='newclose2']").click()
-browser.find_element(By.PARTIAL_LINK_TEXT,"Login").click()
+browser.find_element(By.XPATH, "//a[@class='newclose']").click()
+browser.find_element(By.XPATH, "//a[@class='newclose2']").click()
+browser.find_element(By.PARTIAL_LINK_TEXT, "Login").click()
 
 browser.switch_to.window(browser.window_handles[1])
-browser.find_element(By.XPATH,"//div[text()=' Forgot User ID? ']").click()
+browser.find_element(By.XPATH, "//div[text()=' Forgot User ID? ']").click()
 
-browser.find_element(By.PARTIAL_LINK_TEXT,"select your product type").click()
-browser.find_element(By.PARTIAL_LINK_TEXT,"Credit Card").click()
+browser.find_element(By.PARTIAL_LINK_TEXT, "select your product type").click()
+browser.find_element(By.PARTIAL_LINK_TEXT, "Credit Card").click()
 time.sleep(10)
-#automate date field using javascript
+# automate date field using javascript
 browser.execute_script("document.querySelector('#bill-date-long').value='24/4/2024'")
-#browser.find_element(By.XPATH,"//input[@value='PROCEED']").click()
+# browser.find_element(By.XPATH,"//input[@value='PROCEED']").click()
 time.sleep(5)
 browser.quit()
